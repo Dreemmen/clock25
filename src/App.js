080195843e7714565bbf95b51908bb425ff57815
ArnVig <s-word@inbox.lv>
2023-03-25 19:32:31 +0200

    added styling. tweaked so audio play/preload

--- src/App.js
+++ src/App.js
@@ -3,41 +3,58 @@ import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 import { observer } from 'mobx-react-lite';
-import { clockStore } from './ClockStore';
+import ClockStore from './ClockStore';
 
-console.log(clockStore.decrease_break)
-
-
+const clockStore = new ClockStore()
+    
 function App() {
   return (
-    <div className="App container" >
-      <div className='row'>
+    <div className="App container w-100" >
+      <div className='row justify-content-center'>
 
-        <div className='col-6'>
+        <div className='col-2'>
           <div className='row'>
-            <div id="break-label">break label</div>
+            <div id="break-label" className='break-label'>Break setup</div>
           </div>
             <div className='row'>
-              <div id="break-decrement" className='col-4' onClick={clockStore.decrease_break}>decrease break</div>
-              <div id="break-length" className='col-4'>{clockStore.breakLength}</div>
-              <div id="break-increment" className='col-4' onClick={clockStore.increase_break}>increase break</div>
+              <div id="break-decrement" className='col-4 button1' onClick={() => clockStore.decrease_break()}>Decrease break</div>
+              <div id="break-length" className='col-4 num-display'>{clockStore.breakLength}</div>
+              <div id="break-increment" className='col-4 button1' onClick={() => clockStore.increase_break()}>Increase break</div>
             </div>
         </div>
 
-        <div className='col-6'>
+        <div className='col-2 margin-left-sm'>
           <div className='row'>
-            <div id="session-label">session label</div>
+            <div id="session-label" className='session-label'>Session setup</div>
           </div>
           <div className='row'>
-            <div id="session-decrement" className='col-4'>decrease session</div>
-            <div id="session-length" className='col-4'>{clockStore.sessionLength}</div>
-            <div id="session-increment" className='col-4'>increase session</div>
+            <div id="session-decrement" className='col-4 button1' onClick={() => clockStore.decrease_session()}>Decrease session</div>
+            <div id="session-length" className='col-4 num-display'>{clockStore.sessionLength}</div>
+            <div id="session-increment" className='col-4 button1' onClick={() => clockStore.increase_session()}>Increase session</div>
           </div>
         </div>
 
       </div>
+      <div className='row justify-content-center'>
+
+        <div className='col-2 main-display'>
+          <div id="timer-label" className={"display-"+((clockStore.isBreak)?'break':'session')}>{(clockStore.isBreak)?'Break':'Session'}</div>
+          <div id="time-left" className='time-left'>{clockStore.time}</div>
+        </div>
+        
+      </div>
+      <div className='row justify-content-center'>
+        
+        <div className='col-1'>
+          <div id="start_stop" className='button1' onClick={clockStore.start_stop}>Start / Stop</div>
+        </div>
+        <div className='col-1'>
+          <div id="reset" className='button1' onClick={() => {clockStore.resetClock()}}>Reset</div>
+        </div>
+
+      </div>
+      <audio id="beep" onCanPlay={(event) => clockStore.loadAudio(event)} src="https://audio-previews.elements.envatousercontent.com/files/177328289/preview.mp3?response-content-disposition=attachment%3B+filename%3D%22A3TMECN-beep.mp3%22"/>
     </div>
   );
 }
-
 export default observer(App);
--- src/ClockStore.js
+++ src/ClockStore.js
@@ -1,24 +1,136 @@
-import { makeAutoObservable } from "mobx"
+import { autorun, makeObservable, observable, computed, action } from "mobx"
 
 class ClockStore {
-    breakLength = 5
-    sessionLength = 25
+    breakLength = 0;
+    sessionLength = 0;
+    __date = new Date(0)
+    isTicking = false
+    isBreak = false
+    isSixty = false
+    timer = null
+    time = '25:00'
+    beep = new Audio
+ 
+    constructor() {
+        makeObservable(this, {
+            breakLength: observable,
+            sessionLength: observable,
+            __date: true,
+            isTicking: true,
+            isBreak: true,
+            beep: true,
+            timer: observable,
+            start_stop: action,
+            resetClock: action,
+            increase_break: action,
+            decrease_break: action,
+            increase_session: action,
+            decrease_session: action,
+            check_time: action,
+            getTime: computed,
+            time: observable
+        })
+        this.resetClock() 
+        autorun(() => {
+            if(this.time == '00:00'){
+                this.beep.target.volume = 1
+                this.beep.target.currentTime = 0;
+                this.beep.target.play()
+            }
+        })
+    }
+    
+    get getTime(){
+        let sixtyFix = '0'
+        if(this.isSixty){
+            sixtyFix = '6'
+            this.isSixty = false
+        }
+        return (
+            //((this.__date.getMinutes()=='00')?'6':'0')
+            ((this.__date.getMinutes() < 10)?sixtyFix+this.__date.getMinutes() : this.__date.getMinutes())
+            + ":"
+            +((this.__date.getSeconds() < 10)?'0'+this.__date.getSeconds() : this.__date.getSeconds())
+            )
+    }
 
-    constructor(){
-        makeAutoObservable(this)
+    loadAudio(audio){
+        this.beep = audio
+        this.beep.target.play()
+        this.beep.target.volume = 0
+    }
+
+    check_time = () => {
+        if(this.getTime=='00:00'){
+            this.__date.setMinutes((!this.isBreak)?this.breakLength:this.sessionLength)
+            this.__date.setSeconds(1)
+            this.isBreak = !this.isBreak
+            if(this.beep.oncanplay) this.beep.play()
+        }
+            this.time = this.getTime
+    }
+    make_time = (minutes) => {
+        if(minutes==60){
+            this.isSixty = true
+        }
+        clearInterval(this.timer)
+        this.__date.setMinutes(minutes)
+        this.__date.setSeconds(0)
+        this.check_time()
+    }
+
+    start_stop = () => {  
+      if(this.isTicking){
+        clearInterval(this.timer)
+        this.timer = null
+      }else{
+        this.timer = setInterval(()=>{
+          this.__date.setTime(this.__date.valueOf() - 1000)
+          this.check_time()
+        }, 1000)
+        
+      }
+      this.isTicking = !this.isTicking
+    }
+
+    resetClock(){
+        this.breakLength = 5;
+        this.sessionLength = 25
+        this.__date.setMinutes(25)
+        this.__date.setSeconds(0)
+        clearInterval(this.timer)
+        this.isTicking = false
+        this.isBreak = false
+        if(this.beep.target != undefined){
+             this.beep.target.pause()
+             this.beep.target.currentTime = 0;
+        }
+        this.check_time()
     }
 
     increase_break(){
-        this.breakLength = this.breakLength + 1
+        if(this.breakLength <= 59){
+             this.breakLength = this.breakLength + 1
+             if(this.isBreak) this.make_time(this.breakLength)
+        }
     }
     decrease_break(){
-        this.breakLength = this.breakLength - 1
+        if(this.breakLength >=2 ){
+            this.breakLength = this.breakLength - 1
+            if(this.isBreak) this.make_time(this.breakLength)
+        } 
     }
     increase_session(){
-        this.sessionLength = this.sessionLength + 1
+        if(this.sessionLength <= 59){
+            this.sessionLength = this.sessionLength + 1
+            if(!this.isBreak) this.make_time(this.sessionLength)
+        }
     }
     decrease_session(){
-        this.sessionLength = this.sessionLength - 1
+        if(this.sessionLength >=2 ){
+             this.sessionLength = this.sessionLength - 1
+             if(!this.isBreak) this.make_time(this.sessionLength)
+        }
     }
 }
-export const clockStore = new ClockStore()
+export default ClockStore
